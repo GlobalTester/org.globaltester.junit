f0ab1678d9d30786c110f5b5d9e58df6d0524ff4
Martin Boonk <martin.boonk@hjp-consulting.com>
2012-08-20 11:21:20 +0000

    Additional junit tests and fixes for minor bugs.
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.junit/trunk@1188 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/junit/test/JUnitHelperTest.java
@@ -0,0 +1,73 @@
+package org.globaltester.junit.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.globaltester.core.resources.GtResourceHelper;
+import org.globaltester.junit.JUnitHelper;
+import org.junit.Test;
+
+public class JUnitHelperTest {
+
+	@Test
+	public void testEmptyWorkspace() throws CoreException{
+		GtResourceHelper.createEmptyProject("testProject", ResourcesPlugin.getWorkspace().getRoot().getLocationURI());
+		IProject [] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		assertTrue("workspace contains projects", projects.length > 0);
+		JUnitHelper.emptyWorkspace();
+		projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		assertTrue("workspace contains no projects", projects.length == 0);
+	}
+	
+	@Test
+	public void testCreateDefaultTestSpec() throws IOException, CoreException{
+		JUnitHelper.createDefaultTestSpec();
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(JUnitHelper.testSpec);
+		assertTrue(project.exists());
+	}
+	
+	@Test
+	public void testCreateDefaultCardConfig() throws IOException, CoreException{
+		JUnitHelper.createDefaultCardConfig();
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(JUnitHelper.cardConfig);
+		assertTrue(project.exists());
+	}
+	
+	@Test
+	public void testCreateDefaultTestCampaign() throws IOException, CoreException{
+		JUnitHelper.createDefaultCreateDefaultTestCampaign();
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(JUnitHelper.testCampaign);
+		assertTrue(project.exists());
+	}
+	
+	@Test
+	public void testRecursiveDelete() throws IOException{
+		File tempRoot = File.createTempFile("root", "");
+		tempRoot.delete();
+		tempRoot.mkdir();
+		File tempSub = File.createTempFile("sub", "", tempRoot);
+		tempSub.delete();
+		tempSub.mkdir();
+		File tempFile = File.createTempFile("file", "", tempSub);
+		
+		assertTrue("root folder exists", tempRoot.exists());
+		assertTrue("sub folder exists", tempSub.exists());
+		assertTrue("file exists", tempFile.exists());
+		
+		JUnitHelper.recursiveDelete(tempRoot);
+		
+		assertTrue("root folder does not exist", !tempRoot.exists());
+		assertTrue("sub folder does not exist", !tempSub.exists());
+		assertTrue("file does not exist", !tempFile.exists());
+		
+	}
+}
